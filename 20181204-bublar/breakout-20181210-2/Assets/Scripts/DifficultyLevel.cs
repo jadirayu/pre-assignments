1055713380b7c127d972f65102b076c637862b59
avaniyu <yjybxx2@yahoo.com>
2018-12-11 02:11:40 +0100

    Autolayouting bricks.

---
+++ 20181204-bublar/breakout-20181210-2/Assets/Scripts/DifficultyLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DifficultyLevel : MonoBehaviour {
+
+	public GameObject BrickStone;
+	public GameObject BrickGold;
+	public GameObject BrickEmerald;
+
+	public float BallSpeed{
+		get { return ballSpeed;}
+		set{ ballSpeed = value;}
+	}
+	private float ballSpeed;
+
+	// regenerate bricks
+	public void RefreshDifficultyLevel () {
+		if (Level == 1) {
+			BricksAutoLayout (3, 2);
+			ballSpeed = 450f;
+		} else if (Level == 2) {
+			BricksAutoLayout (5, 3);
+			ballSpeed = 500f;
+		} else if (Level == 3) {
+			BricksAutoLayout (8, 5);
+			ballSpeed = 600f;
+		} else {
+			Debug.Log ("Uncorrect difficulty level");
+		}
+	}
+
+	// randomize bricks layout by difficulty level
+	void BricksAutoLayout(int brickGoldNr, int brickEmeraldNr){
+		// TODO: generate multiple random numbers
+	}
+
+	private int Level = 1;
+	private int brickGoldNr;
+	private int brickEmeraldNr;
+}
