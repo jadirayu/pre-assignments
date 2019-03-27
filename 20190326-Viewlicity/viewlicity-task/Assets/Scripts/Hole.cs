a1ef7fdad257538c1f0e7e8b3330a19fa26b7ba5
yu-mao <yjybxx2@yahoo.com>
2019-03-28 02:35:22 +0800

    Viewlicity. Updated with prescriptive hole positions.

--- 20190326-Viewlicity/viewlicity-task/Assets/Scripts/Hole.cs
+++ 20190326-Viewlicity/viewlicity-task/Assets/Scripts/Hole.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+//TODO: add proper comments
 public class Hole : MonoBehaviour
 {
     public float[] Position { get; private set; }
@@ -19,18 +20,19 @@ public class Hole : MonoBehaviour
     }
 
     private float[] GetRandomPosition(float min_x, float max_x, float min_y, float max_y) {
-        float x = Random.Range(min_x, max_x);
-        float y = Random.Range(min_y, max_y);
+        // TODO: adjust /100 to a programmatic way
+        float x = Random.Range(min_x, max_x) / 50f;
+        float y = Random.Range(min_y, max_y) / 50f;
         float[] position = new float[] { x, y };
         return position;
     }
 
-    private static float MAX_VALUE_WIDTH = Screen.width / 2;
+    private static float MAX_VALUE_WIDTH = Screen.width * 0.74f / 2;
     private static float MIN_VALUE_WIDTH = -1 * MAX_VALUE_WIDTH;
-    private static float MAX_VALUE_HEIGHT = Screen.height / 2;
+    private static float MAX_VALUE_HEIGHT = Screen.height * 0.54f / 2;
     private static float MIN_VALUE_HEIGHT = -1 * MAX_VALUE_HEIGHT;
 
-    private static float[,] INITIAL_HOLE_POSITIONS = new float[3, 2] { { 1, 1 }, { 0.5f, 0.5f }, { 0.3f, 0.3f } };
+    private static float[,] INITIAL_HOLE_POSITIONS = new float[3, 2] { { -0.2f * Screen.width, 0 }, { 0.2f * Screen.width, 0.16f * Screen.height }, { 0.2f * Screen.width, -0.16f * Screen.height } };
 
 
 }
--- 20190326-Viewlicity/viewlicity-task/Assets/Scripts/Professional.cs
+++ 20190326-Viewlicity/viewlicity-task/Assets/Scripts/Professional.cs
@@ -8,6 +8,7 @@ public class Professional : MonoBehaviour
     public Button BtnBack;
     public Dropdown DpdnHoles;
     public GameObject PrefabHole;
+    public Canvas Canvas;
 
     private void Awake() {
         this.BtnBack.onClick.AddListener(GoBack);
@@ -15,6 +16,9 @@ public class Professional : MonoBehaviour
         this.DpdnHoles.onValueChanged.AddListener(delegate { DpdnValueChanged(this.DpdnHoles); });
     }
 
+    /// <summary>
+    /// Initialize the first three holes in given positions
+    /// </summary>
     private void Init3Holes() {
         this.holes = new List<Hole>();
         this.holeInstances = new List<GameObject>();
@@ -34,14 +38,32 @@ public class Professional : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Add a hole 
+    /// in a given position if it's one of the first three holes
+    /// in a random position otherwise
+    /// </summary>
+    /// <param name="isDeletable">is one of the first three holes</param>
+    /// <param name="nr">the number of the hole</param>
     private void AddHole(bool isDeletable, int nr = -1) {
         Hole newHole = new Hole(isDeletable, nr);
         this.holes.Add(newHole);
         RefreshDropdown();
-        GameObject holeInstance = (GameObject)Instantiate(PrefabHole, new Vector2(newHole.Position[0], newHole.Position[1]), Quaternion.identity);
-        this.holeInstances.Add(holeInstance);
+
+        if (isDeletable) {
+            GameObject holeInstance = (GameObject)Instantiate(PrefabHole, new Vector3(this.Canvas.transform.position.x + newHole.Position[0],
+            this.Canvas.transform.position.y + newHole.Position[1], 0), Quaternion.identity);
+            holeInstance.transform.SetParent(this.Canvas.transform);
+            this.holeInstances.Add(holeInstance);
+        }
+        
+        //Debug.Log("x: " + newHole.Position[0]  + " y: " + newHole.Position[1]);
     }
 
+    /// <summary>
+    /// Delete a hole
+    /// </summary>
+    /// <param name="index">index of the selected hole from the dropdown menu</param>
     private void DeleteHole(int index) {
         if (this.holes[index].IsDeletable)
         {
