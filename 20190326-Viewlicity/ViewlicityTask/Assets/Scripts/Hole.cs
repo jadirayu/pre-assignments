51c50dfabac420e672e0e3c704dfcaabeeecf3ec
yu-mao <yjybxx2@yahoo.com>
2019-03-27 15:49:19 +0800

    Viewlicity. Added add new hole function.

--- 20190326-Viewlicity/ViewlicityTask/Assets/Scripts/Hole.cs
+++ 20190326-Viewlicity/ViewlicityTask/Assets/Scripts/Hole.cs
@@ -3,7 +3,7 @@
 public class Hole : MonoBehaviour
 {
     public float[] Position { get; private set; }
-    public bool IsDisplayed { get; set; }
+    //public bool IsDisplayed { get; set; }
 
     public Hole(bool isDeletable, int nr = -1)
     {
@@ -18,10 +18,10 @@ public class Hole : MonoBehaviour
             }
         }
 
-        this.IsDisplayed = true;
+        //this.IsDisplayed = true;
     }
 
-    // TODO: make unrepetitive positions
+    // TODO: make unrepetitive random positions
     private float[] GetRandomPosition()
     {
         float x = Random.Range(MIN_VALUE, MAX_VALUE);
@@ -30,7 +30,8 @@ public class Hole : MonoBehaviour
         return position;
     }
 
-    private static float MAX_VALUE = (float)100.0;
-    private static float MIN_VALUE = (float)1.0;
-    private static float[,] INITIAL_HOLE_POSITIONS = new float[3, 2] { { 1, 1 }, { 2, 2 }, { 3, 3 } };
+    // TODO: adjust the below 3 values by the given light-green area & initial hole positions
+    private static float MAX_VALUE = (float)1.0;
+    private static float MIN_VALUE = (float)0.0;
+    private static float[,] INITIAL_HOLE_POSITIONS = new float[3, 2] { { 1, 1 }, { 0.5f, 0.5f }, { 0.3f, 0.3f } };
 }
--- 20190326-Viewlicity/ViewlicityTask/Assets/Scripts/ProfessionalScreen.cs
+++ 20190326-Viewlicity/ViewlicityTask/Assets/Scripts/ProfessionalScreen.cs
@@ -3,49 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-// TODO: figure out model, view model architecture
 public class ProfessionalScreen : MonoBehaviour
 {
     public Button BtnBack;
-    public Button BtnAddHole;
-
-    void Awake()
-    {
-        this.holesViewModel = new HolesViewModel();
-        string[] txtList = holesViewModel.FormatText();
-        // TODO: programmatically generate the list
-    }
+    public Dropdown Dropdown;
+    public Transform PrefabHole;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.dropdown = GetComponent<Dropdown>();
-        this.dropdown.options.Clear();
-        foreach (Hole hole in this.holes)
-        {
-            dropdown.options.Add(new Dropdown.OptionData("a hole"));
-        }
+        BtnBack.onClick.AddListener(GoToMain);
+        Init3Holes();
+        this.Dropdown.onValueChanged.AddListener(delegate { DropdownValueChanged(this.Dropdown); });
+        this.indexPrevSelectedHole = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        BtnBack.onClick.AddListener(GoToMain);
-        BtnAddHole.onClick.AddListener(AddHole);
     }
 
-    private void GoToMain()
+    private void GoToMain() => Application.LoadLevel("MainScreen");
+
+    /// <summary>
+    /// Initialize three holes & dropdown menu
+    /// </summary>
+    private void Init3Holes()
     {
-        Application.LoadLevel("MainScreen");
+        this.holes = new List<Hole>();
+        for (int i = 0; i < 3; i++)
+        {
+            AddNewHole(false, i);
+        }
+        RefreshDropdown(this.Dropdown);
     }
 
-    private void AddHole()
+    private void DropdownValueChanged(Dropdown dropdown)
     {
-        this.holesViewModel.AddHole();
+        if (dropdown.value == this.holes.Count)
+        {
+            AddNewHole(true);
+        }
+        else
+        {
+            SelectExistingHole(dropdown.value);
+        }
     }
 
-    private HolesViewModel holesViewModel;
-    private Dropdown dropdown;
+    // add a hole from the dropdown
+    private void AddNewHole(bool isDeletable, int nr = -1)
+    {
+        Hole newHole = new Hole(isDeletable, nr);
+        this.holes.Add(newHole);
+        RefreshDropdown(this.Dropdown);
+        Instantiate(PrefabHole, new Vector3(newHole.Position[0], newHole.Position[1], -8.5f), Quaternion.identity);
+    }
+
+    // change the dropdown appearance for a confirmed step "Delete Hole: X?"
+    private void SelectExistingHole(int indexSelectedHole)
+    {
+        //if (this.indexPrevSelectedHole == indexSelectedHole)
+        //{
+        //    DeleteHole(indexSelectedHole);
+        //}
+        //else
+        //{
+        //    this.Dropdown.options[indexSelectedHole] = new Dropdown.OptionData("Delete Hole: " + (char)(indexSelectedHole + 1) + "?");
+        //}
+        //this.indexPrevSelectedHole = indexSelectedHole;
+    }
+
+    /// <summary>
+    /// Refresh the dropdown with the last option "Add New"
+    /// </summary>
+    /// <param name="dropdown">gameobject Dropdown</param>
+    private void RefreshDropdown(Dropdown dropdown)
+    {
+        this.Dropdown.options.Clear();
+        for (int i = 0; i < this.holes.Count; i++)
+        {
+            this.Dropdown.options.Add(new Dropdown.OptionData(" " + i));
+        }
+        this.Dropdown.options.Add(new Dropdown.OptionData("Add New"));
+        dropdown.RefreshShownValue();
+    }
+
+    // delete a hole from the dropdown, too
+    // TODO: delete a hole by tapping on it and select "delete" from popup context menu
+    private void DeleteHole(int indexDeletingHole)
+    {
+    }
+
+    private void FormatDropdown()
+    {
+    }
+
+
     private List<Hole> holes;
+    private int indexPrevSelectedHole;
 }
